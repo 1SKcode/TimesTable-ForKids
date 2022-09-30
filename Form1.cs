78c53a0b79e60f64005a7bc013ab2ce0d5d77cfe
Александр <ipp387525@gmail.com>
2022-09-30 15:53:06 +0300

    Проверка

--- Form1.cs
+++ Form1.cs
@@ -16,6 +16,7 @@ namespace Таблица_умножения_Forms
 {
     public partial class Form1 : Form
     {
+        //-------------------------------------------//
         public Form1()
         {
             InitializeComponent();
